ff77b0f5a6076bf153e468fb8047705a824705cd
MilesJoseph <miles.klingenberg@outlook.com>
2021-05-16 18:12:41 -0700

    new

--- dags/udac_example.py
+++ dags/udac_example.py
@@ -126,7 +126,7 @@ load_artist_dimension_table = SubDagOperator(
 
 
 load_time_dimension_table = SubDagOperator(
-    subdag=load_dimension_subdag(
+    subdag=dimension_subdag(
         parent_dag_name=dag_name,
         task_id="Load_time_dim_table",
         redshift_conn_id="redshift",
